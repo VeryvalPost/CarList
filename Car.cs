bf382bd6a3d62f3d6abdda497941f13bd7deffc6
Veryval <veryval@mail.ru>
2023-04-20 21:55:42 +0300

    Initial commit

---
+++ Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace carSpace
+{
+    class Car
+    {
+        public int id{get;}
+        public String mark {get; set;}
+        public String model;
+        public int year;
+        public String color;
+        public double price;
+        public Boolean aviable;
+
+        public Car(int Id, String Mark, String Model, int Year, String Color, double Price, Boolean Aviable) {
+          id = Id;
+          mark = Mark;
+          model = Model;
+          year = Year;
+          color = Color;
+          price = Price;
+          aviable = Aviable;
+        }
+
+        public void Print() {
+            Console.WriteLine($"{id} {mark} {model} {year} {color} {price} {aviable}");
+        }
+
+    }
+
+
+
+
+
+
+}
+
+
---
+++ Main.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace carSpace
+{
+    class carSpace
+    {   
+        static List<Car> carList = new();
+        static int id = 0; 
+        //static Car test = new Car(1, "Tesla", "Model", 2021 , "Red" ,1000, true);
+        
+        static void Main(string[] args)
+        {
+            Boolean exit = true;  
+            while (exit) {
+                Console.WriteLine("Выберите необходимое действие:");
+                Console.WriteLine("1: Добавить автомобиль ADD");
+                Console.WriteLine("2: Удалить автомобиль REMOVE");
+                Console.WriteLine("3: Найти автомобиль по марке: FIND");
+                Console.WriteLine("4: Вывести весь список автопарка PRINT");
+                Console.WriteLine("5: Выход EXIT");
+                int action = Convert.ToInt16(Console.ReadLine());
+                switch (action){
+                    case 1: 
+                         carList.Add(AddToList());
+                         break;
+                    case 2: 
+                         
+                         DeleteFromList();
+                         break;
+                    case 3: 
+                         FindCar();
+                         break;   
+
+                    case 4: 
+                         foreach (Car elem in carList) {
+                            elem.Print();
+                         }
+                         break;                               
+                    case 5: 
+                         exit = false;
+                         break;
+                    
+                               
+                }
+            } 
+
+        }
+
+        static Car AddToList () {
+            Console.WriteLine ("Введите новый автомобиль в формате:  Tesla Model 2021 Red 1000 True");
+            String strCar = Console.ReadLine();
+            String[] arrCar = strCar.Split(" ");
+            id = id + 1;
+            Car newCar = new Car(id, arrCar[0], arrCar[1], Convert.ToInt32(arrCar[2]), arrCar[3], Convert.ToDouble(arrCar[4]), true);
+            return newCar;
+        }
+
+        static void DeleteFromList () {
+            Console.WriteLine ("Введи номер позиции для удаления ###");
+            int index = Convert.ToInt16(Console.ReadLine());
+            carList.RemoveAt(index-1); 
+            Console.WriteLine ("Данная позиция удалена");
+        }
+
+        static void FindCar () {
+            Console.WriteLine ("Введи марку авто");
+            String mark = Console.ReadLine();
+
+            foreach(Car car in carList){
+                if (car.mark.Equals(mark)){
+                    car.Print();
+                }
+            }
+        }
+
+    }
+
+
+
+
+}
